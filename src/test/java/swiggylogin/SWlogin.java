3f0306f663d0bf3a33546b3901e457a261f633ac
kumarraj96 <kumarraj1239@yahoo.com>
2024-07-29 00:50:56 +0530

    first time push

---
+++ src/test/java/swiggylogin/SWlogin.java
@@ -0,0 +1,21 @@
+package swiggylogin;
+
+public class SWlogin {
+	
+	
+	private void food() {
+		// TODO Auto-generated method stub
+
+	}
+	private void cake() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void honey() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
